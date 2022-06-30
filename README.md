4ac04c6a212fd0c32d913d15fc5f5d25cd9f468f
rjaggannagari <rjaggannagari@vt.edu>
2022-06-30 11:55:29 -0400

    few commands

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# DiscordBot
+Directions on how to use the bot correctly
+
+Commands(rememeber the - in front)
+
+1. -msping, returns the users ping in ms
+
+2. -flipcoin, returns a random choice of heads or tails
+
+3. -telljoke, return a random joke from a database of jokes
--- first_bot.py
+++ first_bot.py
@@ -4,6 +4,8 @@ import discord
 import random
 import os
 from discord.ext import commands
+import pandas
+import numpy
 
 client = commands.Bot(command_prefix = '-')
 
@@ -11,6 +13,24 @@ client = commands.Bot(command_prefix = '-')
 @client.event
 async def on_ready():
     print('Basic discord bot is ready to use')
+
+@client.command()
+async def clear(ctx, amount = 5):
+    await ctx.channel.purge(limit = amount)
+
+# This checks to see if a specfic message is sent
+@client.event
+async def on_message(message):
+    discord_username = str(message.author).split("#")[0]
+    channel_name = str(message.channel.name)
+
+    if message.author == client.user:
+        return
+
+    # If hi or hello are said in the welcome chat return hi along with their username
+    # Sends a basic message back to users
+    if 'hi' in message.content or 'hello' in message.content:
+        await message.channel.send(f'Hi , {discord_username}, hope you are having a great day!')
 
 # This command gives the clients current ping, to use this command just call -msping
 @client.command()
@@ -22,6 +42,20 @@ async def msping(ctx):
 async def flipcoin(ctx):
     choices = ['Heads', 'Tails']
     await ctx.send(f'It is {random.choice(choices)}')
+
+# This command looks through a csv file with over 20000 jokes and randomly chooses one
+@client.command()
+async def telljoke(ctx):
+    jokes_filename = 'shortjokes.csv'
+    filtered_jokes = pandas.read_csv(jokes_filename, usecols=['Joke'])
+    jokes_array = numpy.array(filtered_jokes)
+    await ctx.send(f'{random.choice(jokes_array)}')
+
+@client.command()
+async def message(ctx, user:discord.member, *, message = None):
+    message = "welcome"
+    embed = discord.Embed(title = message)
+    await user.send(embed = embed)
 
 # Each bot has their unique token so I have placed mine in an .env file
 client.run(os.getenv('TOKEN'))
